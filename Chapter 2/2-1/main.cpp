3978808b29556498bec64c0d69b217cb621b9781
hym_dn <yameng_he@aliyun.com>
2018-03-04 21:15:48 +0800

    提交源码2-1

---
+++ Chapter 2/2-1/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Standard Clib: "<<__STDC_HOSTED__<<endl; // Standard Clib:1
+    cout<<"Standard C: "<<__STDC__<<endl; // Standard C:1
+    //cout<<"C Standard version: "<<__STDC_VERSION__<<endl;
+    cout<<"ISO/IEC"<<__STDC_ISO_10646__<<endl; // ISO/IEC 200009
+}
+
+// 编译选项 :g++ -std=c++11 2-1-1.cpp
