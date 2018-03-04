f4e1f244d619e93ddebda4037c583358b53eeb4f
hym_dn <yameng_he@aliyun.com>
2018-03-04 22:03:23 +0800

    提交源码2-4

---
+++ Chapter 2/2-4/main.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+#define LOG(...) {\
+    fprintf(stderr,"%s: Line %d;\t",__FILE__,__LINE__);\
+    fprintf(stderr,__VA_ARGS__);\
+    fprintf(stderr,__VA_ARGS__);\
+    fprintf(stderr,"\n");\
+}
+
+int main(){
+    int x=3;
+    // 一些代码...
+    LOG("x=%d",x,4); // 2-1-5.cpp: Line 12: x=3
+}
+
+// 编译器选项: g++ -std=c++11 2-1-5.cpp
