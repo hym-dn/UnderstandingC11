ec16802a99463c3df9e164a21eee3678de4d8813
hym_dn <yameng_he@aliyun.com>
2018-03-11 20:01:23 +0800

    提交源码2-5

---
+++ Chapter 2/2-5/main.cpp
@@ -0,0 +1,14 @@
+#include<climits>
+#include<cstdio>
+using namespace std;
+
+int main(){
+    long long ll_min=LLONG_MIN;
+    long long ll_max=LLONG_MAX;
+    unsigned long long ull_max=ULLONG_MAX;
+    printf("min of long long: %lld\n",ll_min); // min of long long
+    printf("max of long long: %lld\n",ll_max); // max of long long
+    printf("max of unsigned long long: %llu\n",ull_max); // max of unsigned long long
+}
+
+// 编译选项： g++ -std=c++11 2-2-1.cpp
