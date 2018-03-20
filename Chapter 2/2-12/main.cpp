8f0e9a06313adaa8a0b046f5adc514351ecc61d7
hym_dn <yameng_he@aliyun.com>
2018-03-20 22:35:42 +0800

    提交源码2-12

---
+++ Chapter 2/2-12/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void Throw(){throw 1;}
+void NoBlockThrow(){Throw();}
+void BlockThrow()noexcept{Throw();}
+
+int main(){
+    try{
+        Throw();
+    }catch(...){
+        cout<<"Found throw."<<endl; // Found throw
+    }
+    try{
+        NoBlockThrow();
+    }catch(...){
+        cout<<"Throw is not blocked."<<endl; // Throw is not blocked.
+    }
+    try{
+        BlockThrow(); // teminate called after throwing an instance of 'int'
+    }catch(...){
+        cout<<"Found throw 1."<<endl;
+    }
+}
