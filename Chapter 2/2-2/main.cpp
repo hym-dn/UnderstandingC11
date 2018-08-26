6705776d15ae10a442368fcfb5e0e3afe0cbe6db
yameng_he <yameng_he@aliyun.com>
2018-08-26 19:23:04 +0800

    完善源码2-2

--- Chapter 2/2-2/main.cpp
+++ Chapter 2/2-2/main.cpp
@@ -5,6 +5,9 @@ using namespace std;
 const char *hello(){return __func__;}
 const char *world(){return __func__;}
 
+/**
+ * 打印调用函数名字
+ */
 int main(){
     cout<<hello()<<","<<world()<<endl; //hello,world
 }
