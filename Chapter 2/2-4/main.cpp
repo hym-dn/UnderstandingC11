bcdd576aebfcf07747cb1f2d1a595572fa7992b0
yameng_he <yameng_he@aliyun.com>
2018-08-26 22:04:33 +0800

    提交源码2-4

--- Chapter 2/2-4/main.cpp
+++ Chapter 2/2-4/main.cpp
@@ -3,14 +3,16 @@
 #define LOG(...) {\
     fprintf(stderr,"%s: Line %d;\t",__FILE__,__LINE__);\
     fprintf(stderr,__VA_ARGS__);\
-    fprintf(stderr,__VA_ARGS__);\
     fprintf(stderr,"\n");\
 }
 
+/**
+ * 利用宏可变长参数记录日志
+ */
 int main(){
     int x=3;
     // 一些代码...
-    LOG("x=%d",x,4); // 2-1-5.cpp: Line 12: x=3
+    LOG("x=%d",x); // 2-1-5.cpp: Line 12: x=3
 }
 
 // 编译器选项: g++ -std=c++11 2-1-5.cpp
