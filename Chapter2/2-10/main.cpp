cceaccfbe7fe0b0f22f022a18e8eef511a56d86a
yameng_he <yameng_he@aliyun.com>
2018-08-29 16:58:00 +0800

    完善源码2-10

--- Chapter2/2-10/main.cpp
+++ Chapter2/2-10/main.cpp
@@ -1,7 +1,8 @@
 #include<cassert>
-#include<assert.h>
 using namespace std;
 
 static_assert(sizeof(int)==8,"This 64-bit machine should follow this!");
 
 int main(){return 0;}
+
+// 编译选项： g++ -std=c++11 2-5-5.cpp
