5343e644fb6952540ca7c3c48831358eb735d3ef
yameng_he <yameng_he@aliyun.com>
2018-09-02 21:11:14 +0800

    提交源码2-20

---
+++ Chapter2/2-20/main.cpp
@@ -0,0 +1,12 @@
+class P;
+
+template<typename T> class People{
+    friend T;
+};
+
+int main(){
+    People<P> PP; // 类型P在这里是People类型的友元
+    People<int> Pi; // 对于int类型模板参数,友元声明被忽略
+}
+
+// 编译选项： g++ -std=c++11 2-9-2.cpp
